fb7c0d78a28de33ae91a1ac381b31a28518df08f
administracion181 <administracion@hotelpetitcba.com>
2026-02-23 20:03:41 -0300

    index.js

--- index.js
+++ index.js
@@ -4,52 +4,29 @@ import axios from "axios";
 const app = express();
 app.use(express.json());
 
-const VERIFY_TOKEN = "mi_token_seguro_123";
-const WHATSAPP_TOKEN = EAFyKs5Y5LgEBQxaATr5K3s5LzjHi2txkC8m2HJTj9vuCzLaelytBOJWBTgAItfZCp7lVt99AJ338H0kZAPo5a573zieqHZAgakF2bG01QJxTHglWAxdY3DNmb4vRkXTzvD56pi3ns4tZCNCgAxpZBUkqVOZCtDDSgrZAnG0ntibKg9QRqDSVpgQdOq3h25fgJ2ZAy3ZBXH30cYelvfJaf7vKQUejfMZA5vVzS02UL0Ed7o5IjdgcmlJZAZCfrug5mdaLz2GoRZAUZCJxZC29xRQXFJz6UOhtxld
-const PHONE_NUMBER_ID = 964082273463562
-// Verificación webhook
+app.get("/", (req, res) => {
+  res.send("Servidor activo 🚀");
+});
+
 app.get("/webhook", (req, res) => {
   const mode = req.query["hub.mode"];
   const token = req.query["hub.verify_token"];
   const challenge = req.query["hub.challenge"];
 
-  if (mode === "subscribe" && token === VERIFY_TOKEN) {
-    return res.status(200).send(challenge);
+  if (mode === "subscribe" && token === "mi_token_seguro_123") {
+    res.status(200).send(challenge);
   } else {
-    return res.sendStatus(403);
+    res.sendStatus(403);
   }
 });
 
-// Recibir mensajes
+app.post("/webhook", (req, res) => {
+  console.log("Mensaje recibido");
+  res.sendStatus(200);
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log("Servidor activo en puerto " + PORT);
 });
-  const message = req.body.entry?.[0]?.changes?.[0]?.value?.messages?.[0];
-
-  if (message) {
-    const from = message.from;
-    const text = message.text?.body;
-
-    await axios.post(
-      `https://graph.facebook.com/v22.0/${PHONE_NUMBER_ID}/messages`,
-      {
-        messaging_product: "whatsapp",
-        to: from,
-        type: "text",
-        text: { body: "Recibí: " + text }
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${WHATSAPP_TOKEN}`,
-          "Content-Type": "application/json"
-        }
-      }
-    );
-  }
-
-  res.sendStatus(200);
-});
-
-app.listen(3000);
